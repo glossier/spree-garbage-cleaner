82319de77ea132c65e9d7396e41e6c9ab8933f25
braidn <braden.douglass@gmail.com>
2015-03-26 14:23:51 -0400

    Clean Up Order Spec
    Clean the order spec up to align with Rubocop recommendations.

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -3,47 +3,51 @@ require 'spec_helper'
 describe Spree::Order do
   let(:ordered_on) { Spree::GarbageCleaner::Config.cleanup_days_interval }
 
-  context "class methods" do
+  context 'class methods' do
     before do
-      @order_one = create(:order, :created_at => (ordered_on+rand(10)).days.ago, :completed_at => nil)
-      @order_two = create(:order, :created_at => (ordered_on+rand(10)).days.ago, :completed_at => nil)
+      @order_one = create(:order, created_at: (ordered_on + rand(10)).days.ago,
+                                  completed_at: nil)
+      @order_two = create(:order, created_at: (ordered_on + rand(10)).days.ago,
+                                  completed_at: nil)
       @order_three = create(:order)
     end
 
-    it "has a garbage finder method" do
+    it 'has a garbage finder method' do
       Spree::Order.garbage.should == [@order_one, @order_two]
     end
 
-    it "has a method to destroy garbage" do
+    it 'has a method to destroy garbage' do
       Spree::Order.destroy_garbage.should == [@order_one, @order_two]
       Spree::Order.garbage.count.should == 0
       Spree::Order.all.should include(@order_three)
     end
   end
 
-  context "instance methods" do
-    it "has a method that tells if order is garbage" do
+  context 'instance methods' do
+    it 'has a method that tells if order is garbage' do
       order = build(:order)
       order.should respond_to(:garbage?)
     end
 
-    it "is garbage if not completed and past cleanup_days_interval" do
-      order = build(:order, :created_at => ordered_on.days.ago, :completed_at => nil)
+    it 'is garbage if not completed and past cleanup_days_interval' do
+      order = build(:order, created_at: ordered_on.days.ago, completed_at: nil)
       order.garbage?.should be_truthy
     end
 
-    it "is not garbage if not completed and not past cleanup_days_interval" do
-      order = build(:order, :created_at => (ordered_on-1).days.ago, :completed_at => nil)
+    it 'is not garbage if not completed and not past cleanup_days_interval' do
+      order = build(:order, created_at: (ordered_on - 1).days.ago,
+                            completed_at: nil)
       order.garbage?.should be_falsey
     end
 
-    it "is not garbage if completed and past cleanup_days_interval" do
-      order = build(:order, :created_at => ordered_on.days.ago, :completed_at => Time.now)
+    it 'is not garbage if completed and past cleanup_days_interval' do
+      order = build(:order, created_at: ordered_on.days.ago,
+                            completed_at: Time.now)
       order.garbage?.should be_falsey
     end
 
-    it "is not garbage if completed and not past cleanup_days_interval" do
-      order = build(:order, :completed_at => Time.now)
+    it 'is not garbage if completed and not past cleanup_days_interval' do
+      order = build(:order, completed_at: Time.now)
       order.garbage?.should be_falsey
     end
   end
