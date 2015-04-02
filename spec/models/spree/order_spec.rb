04d96a2b05b63ec2e9315142c923332dd3760959
braidn <braden.douglass@gmail.com>
2015-04-02 11:11:24 -0400

    Update Order Spec/Order Cleanup
    Make sure to test that the order garbage cleanup is removing all of the
    extraneous items of the order. Like the following:
    
    1. Payments
    2. Shipments
    3. Line Items
    4. Adjustments

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -6,9 +6,9 @@ describe Spree::Order do
   context 'class methods' do
     before do
       @order_one = create(:order, created_at: (ordered_on + rand(10)).days.ago,
-                                  completed_at: nil)
+                                                  completed_at: nil)
       @order_two = create(:order, created_at: (ordered_on + rand(10)).days.ago,
-                                  completed_at: nil)
+                                                  completed_at: nil)
       @order_three = create(:order)
     end
 
@@ -49,6 +49,38 @@ describe Spree::Order do
     it 'is not garbage if completed and not past cleanup_days_interval' do
       order = build(:order, completed_at: Time.now)
       expect(order.garbage?).to be_falsey
+    end
+  end
+
+  context 'associated objects' do
+    let(:order) { create(:order, created_at: (ordered_on + rand(10)).days.ago,
+                                 completed_at: nil)
+    }
+    let(:payment) { create :payment }
+    let(:adjustment) { create :adjustment, order: order }
+    let(:shipment) { create :shipment }
+    let(:line_item) { create :line_item, order: order  }
+
+    subject { Spree::Order.destroy_garbage }
+
+    it 'removes line_items' do
+      order.line_items << line_item
+      subject
+      expect(Spree::LineItem.all).to_not include(order.line_items)
+    end
+    it 'removes adjustments' do
+      subject
+      expect(Spree::Adjustment.all).to_not include(order.adjustments)
+    end
+    it 'removes payments' do
+      order.payments << payment
+      subject
+      expect(Spree::Payment.all).to_not include(order.payments)
+    end
+    it 'removes shipments' do
+      order.shipments << shipment
+      subject
+      expect(Spree::Shipment.all).to_not include(order.shipments)
     end
   end
 end
