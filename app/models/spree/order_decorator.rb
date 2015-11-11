25c94db99c2bdfa28d142c45a4b7dc4846ba53c1
Doug Johnston <doug@ricksteves.com>
2015-11-11 10:08:56 -0800

    File cleanup

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,7 +6,7 @@ module Spree
 
     def self.garbage
       garbage_after = Spree::GarbageCleaner::Config.cleanup_days_interval
-      self.incomplete.where("created_at <= ?", garbage_after.days.ago)
+      self.incomplete.where('created_at <= ?', garbage_after.days.ago)
     end
 
     def garbage?
